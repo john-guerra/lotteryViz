29cd7bc323589d811ff954bee48dc29f7def8dc6
John Alexis Guerra Gómez <john.guerra@gmail.com>
2017-08-15 11:25:02 -0500

    Adding main files

--- main.js
+++ main.js
@@ -1,84 +1,129 @@
+/* global d3 */
 var options = [
-	"ANDRADE BARON, LUISA",
-	"AVILA MATALLANA, LAURA C.",
-	"BALLESTEROS CUCAITA, MARIA P.",
-	"BARRENECHE, DANIELA S.",
-	"BOTIA TARAZONA, NICOLAS",
-	"CARVAJAL DE LEON, CARLOS A.",
-	"HERNANDEZ CARDOSO, DANNA V.",
-	"IBARRA LIZCANO, MARCO A.",
-	"JORDAN LEAL, JUANPABLO",
-	"MARROQUIN DIAZ, ANGIE M.",
-	"ORTIZ, LUIS F.",
-	"PAEZ JIMENEZ, CARLOS E.",
-	"PALENCIA RAMIREZ, DANIELA",
-	"RAMIREZ AMARIZ, ANDREA C.",
-	"RAMIREZ GAMBOA, JUAN C.",
-	"RENGIFO ESPINOSA, JUAN F.",
-	"ROJAS PINZON, FELIPE A.",
-	"ROMERO SALAZAR, JORGE A.",
-	"TORO MARQUEZ, CAROLINA",
-	"TORRES ALDANA, ANDRES F.",
-	"VELASQUEZ MEJIA, DAVID",
-	"VERA FONSECA, MARIA M.",
-	"VILLAMIL RUIZ, DIEGO J.",
-	"VILLAMIZAR VILLAMIZAR, JESUS D."
+  "ALFONSO SANCHEZ  CAMILO ALEJANDRO",
+  "ARCINIEGAS HURTADO JUAN SEBASTIAN",
+  "ARDILA CUADRADO  JOHN EDISSON",
+  "BAGES PRADA  JUAN CAMILO",
+  "BAQUERO MERCHAN  SAMUEL",
+  "BAUTISTA MORA  JAIRO EMILIO",
+  "BELLO JIMENEZ  LAURA NATALIA",
+  "BOHORQUEZ SANCHEZ  BRANDON ANDRES",
+  "CABELLO AGÜERO EDUARDO ARTURO",
+  "CALDERON PACHON  HECTOR JOSE",
+  "CAMACHO CARO JUAN PABLO",
+  "CASTRO VARON JUAN JOSE",
+  "CHAPARRO MACHETE JUAN ESTEBAN",
+  "CORDOBA BORJA  ALEJANDRO",
+  "CUBILLOS SANCHEZ DAVID FERNANDO",
+  "DALEL RUEDA  ESTEBAN",
+  "DE LA VEGA FERNANDEZ ANTONIO JUAN",
+  "DUARTE BERNAL  IVAN DARIO",
+  "GOMEZ CELIS  JUAN SEBASTIAN",
+  "GONZALEZ ALVAREZ CARLOS EDUARDO",
+  "GUZMAN SARMIENTO JUAN DAVID",
+  "IREGUI CARREÑO FELIPE",
+  "MANTILLA ACOSTA  RAFAEL JOSE",
+  "MARIÑO RODRIGEZ  DANIELA",
+  "MARTINEZ CASTAÑO SEBASTIAN",
+  "PAREDES VALDERRAMA ANDREA JULIANA",
+  "PINZON CAPERA  LADY JERALDYNNE",
+  "PLAZAS MONTAÑEZ  LUIS FELIPE",
+  "POVEDA GOMEZ JULIO ANDRES",
+  "ROBAYO GONZALEZ  SANTIAGO",
+  "RODRIGUEZ BARRAGAN JUAN MANUEL",
+  "RODRIGUEZ RUBIO  DIEGO ANDRES",
+  "ROJAS HERRERA  SANTIAGO",
+  "SANDOVAL CORDERO ANDRES",
+  "SOLANO BELTRAN DIANA CATALINA",
+  "TORRES PINZON  JOAN DAVID",
+  "TROCONIS GUIA  JAVIER ANTONIO",
+  "VELANDIA GONZALEZ  ESNEIDER",
+  "VERDUGO QUEVEDO  ANDRES FELIPE",
+  "ZUCCHET CARDENAS DIEGO ALEJANDRO"
 ];
+
+var allOptions = options.map(function (d, i) {
+  return {name:d, id:i, drawn:false};
+});
+var optionsLeft = allOptions.map(function (d) { return d; });
+var optionsDrawn = [];
 
 
 var width = 800,
-	height = 800;
+  height = 800,
+  endAngle = 360 - 360/options.length ;
 
 var svg = d3.select("#result")
-	.append('svg')
+  .append('svg')
   .attr('width', width)
   .attr('height', height);
 
 var angleScale = d3.scale.linear()
-	.domain([0, options.length-2])
-	.range([0,350]);
+  .domain([0, options.length-1])
+  .range([0,endAngle]);
 
 
 d3.select("#btnChoose")
-	.on("click", onChoose);
+  .on("click", onChoose);
 
 
 
 function redraw(options) {
-var optionsSel = svg.selectAll(".option")
-	.data(options);
+  var optionsSel = svg.selectAll(".option")
+    .data(options);
 
-	optionsSel.enter()
-		.append("text")
-		.attr("class", "option");
+  optionsSel.enter()
+    .append("text")
+    .attr("class", "option");
 
-	optionsSel
-		// .attr("x", width/2)
-		// .attr("y", height/2)
-		.attr("id", function (d,i) { return "id"+ i; })
-		.text(function(d) { return d; })
-		.transition().duration(1000)
-		.attr("transform", function (d,i ) {
-			return "translate(" + width/2 + "," + height/2  +
-				") rotate(" + angleScale(i) + ")";
-		});
+  optionsSel
+    // .attr("x", width/2)
+    // .attr("y", height/2)
+    .attr("id", function (d) { return "id"+ d.id; })
+    .classed("drawn", function(d) { return d.drawn; })
+    .text(function(d) { return d.name; })
+    .transition().duration(1000)
+    .attr("transform", function (d) {
+      return "translate(" + width/2 + "," + height/2  +
+        ") rotate(" + angleScale(d.id) + ")";
+    });
 
-	optionsSel.exit().remove();
+  optionsSel.exit().remove();
 
 }
 
 
-redraw(options);
+redraw(allOptions);
 
 function onChoose() {
-	var sel = Math.round(Math.random() * options.length);
-	var selAngle = angleScale(sel);
-	angleScale.range([selAngle, selAngle+350]);
-	console.log("#id"+sel);
-	d3.selectAll(".option").style("font-size", "15pt");
-	d3.select("#id" +sel).transition().duration(500)
-		.style("font-size", "50pt")
-		.style("fill", "red");
-	redraw(options);
-	// d3.select("#result").text("Seleccionado = " + options[sel]);
+  var sel = Math.round(Math.random() * optionsLeft.length);
+  var optionSel = optionsLeft.splice(sel, 1)[0];
+  optionSel.drawn = true;
+  optionsDrawn = [optionSel].concat(optionsDrawn);
+  angleScale
+    .range([0, endAngle]);
+  var selAngle = angleScale(optionSel.id);
+
+  console.log("sel="+ sel +" angle="+selAngle + " option " + optionSel.name);
+
+  angleScale.range([-selAngle, endAngle-selAngle]);
+  console.log("#id "+sel);
+  d3.selectAll(".option")
+    .classed("selected", false);
+  redraw(allOptions);
+  console.log("#id" + optionSel.id);
+  d3.select("#id" + optionSel.id)
+    .classed("selected", true);
+
+  var drawn = d3.select("#drawn").selectAll(".drawn")
+    .data(optionsDrawn);
+
+  drawn.enter()
+    .append("p");
+  drawn
+    .attr("class", "drawn")
+    .text(function (d) { return d.name; });
+  drawn.exit().remove();
+
+  // d3.select("#result").text("Seleccionado = " + options[sel]);
 }
